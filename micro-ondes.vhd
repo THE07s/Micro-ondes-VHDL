d3819c492d0829d69c7e50b1af4818190b97577a
Loïc Harry LEENAERTS <loic_leenaerts@reseau.eseo.fr>
2025-01-10 01:31:23 +0100

    Refonte du micro-ondes en un process synchrone unique :
    Passage d’une logique répartie sur plusieurs process à un seul process séquentiel (p_main).
    Évite les erreurs de multi-driver (MDRV-1) sur les signaux secondes, pause, secondes_decalees, etc.
    Corrige les conflits de pilotage.
    
    Justification :
    Auparavant, plusieurs process écrivaient sur les mêmes signaux, provoquant des erreurs de type « multi-driven net » lors de la synthèse. En regroupant toute la logique principale (porte, démarrage, pause, chronomètre, buzzer) dans un seul process synchrone, on évite les conflits de pilotage.

--- micro-ondes.vhd
+++ micro-ondes.vhd
@@ -77,7 +77,7 @@ end TruthTable;
 
 --*****************************************************************************
 --
--- micro-ondes entity+archi
+-- Micro_ondes entity+archi
 --
 --*****************************************************************************
 library ieee;
@@ -86,50 +86,55 @@ use ieee.numeric_std.all;
 
 entity micro_ondes is
     port(
-        clk_i                           : in  std_logic;
-        switches_i                      : in  std_logic_vector(15 downto 0);
-        btn_left_i                      : in  std_logic;
-        btn_center_i                    : in  std_logic;
-        btn_right_i                     : in  std_logic;
-        btn_up_i                        : in  std_logic;
-        btn_down_i                      : in  std_logic;
-        leds_o                           : out std_logic_vector(15 downto 0);
-        disp_segments_n_o               : out std_logic_vector(6 downto 0);
-        -- disp_point_n_o                  : out std_logic;
-        disp_select_n_o                 : out std_logic_vector(3 downto 0)
+        -- Définition de nos ports :
+        clk_i                               : in  std_logic;
+        switches_i                          : in  std_logic_vector(15 downto 0);
+        btn_left_i                          : in  std_logic;
+        btn_right_i                         : in  std_logic;
+        btn_center_i                        : in  std_logic;
+        btn_up_i                            : in  std_logic;
+        btn_down_i                          : in  std_logic;
+
+        leds_o                              : out std_logic_vector(15 downto 0);
+
+        disp_segments_n_o                   : out std_logic_vector(6 downto 0);
+        disp_select_n_o                     : out std_logic_vector(3 downto 0)
     );
 end micro_ondes;
 
 architecture Structural of micro_ondes is
-    signal porte_fermee                 : std_logic;
-    signal debut                        : std_logic;
-    signal fonctionnement               : std_logic := '0';
-    signal pause                        : std_logic := '0';
-    signal fin                          : std_logic;
-    signal magnetron                    : std_logic;
-    signal buzzer_actif                 : std_logic := '0';
-    signal compteur_buzzer              : integer range 0 to 3 := 0;
-    signal seconde                      : integer;
-    signal minute                       : integer;
-    signal secondes                     : integer range 0 to 5999;
-    signal secondes_decalees            : integer range 0 to 5999 := 0;
-    signal dizaine_minute               : integer range 0 to 9;
-    signal unite_minute                 : integer range 0 to 9;
-    signal dizaine_seconde              : integer range 0 to 5;
-    signal unite_seconde                : integer range 0 to 9;
-    signal clk_slow_1s                  : std_logic;
-    signal clk_slow_20ms                : std_logic;
-    signal afficheur_selection          : std_logic_vector(1 downto 0) := "00";
-    signal valeur_afficheur             : integer range 0 to 9;
+
+    -- Clock ralenties 1s & 20ms :
+    signal clk_slow_1s                      : std_logic;
+    signal clk_slow_20ms                    : std_logic;
+
+    -- Signaux internes :
+    signal porte_fermee                     : std_logic := '1';
+    signal fonctionnement                   : std_logic := '0';
+    signal pause                            : std_logic := '0';
+    signal magnetron                        : std_logic := '0';
+    signal buzzer_actif                     : std_logic := '0';
+    signal compteur_buzzer                  : integer range 0 to 3 := 0;
+
+    signal secondes                         : integer range 0 to 5999 := 0;
+    signal secondes_decalees                : integer range 0 to 5999 := 0;
+    signal minute                           : integer range 0 to 99;
+    signal seconde                          : integer range 0 to 59;
+
+    signal dizaine_minute                   : integer range 0 to 9;
+    signal unite_minute                     : integer range 0 to 9;
+    signal dizaine_seconde                  : integer range 0 to 5;
+    signal unite_seconde                    : integer range 0 to 9;
+
+    signal valeur_et_afficheur_selection    : std_logic_vector(1 downto 0) := "00";
+    signal valeur_afficheur                 : integer range 0 to 9;
 
 begin
----------------------------------------------------------------    
--- Divider 1s
----------------------------------------------------------------
+-------------------------------------------------------------------
+-- Implémentation des deux diviseurs de clock :
+-------------------------------------------------------------------
     divider_1s_inst : entity work.CounterModN(Behavioral)
-       generic map(
-            N => 100e6
-        )
+        generic map( N => 100e6 )
         port map(
             clk_i   => clk_i,
             reset_i => '0',
@@ -137,13 +142,9 @@ begin
             value_o => open,
             cycle_o => clk_slow_1s
         );
----------------------------------------------------------------    
--- Divider 20ms
----------------------------------------------------------------
+
     divider_20ms_inst : entity work.CounterModN(Behavioral)
-        generic map(
-            N => 2e6
-        )
+        generic map( N => 2e6 )
         port map(
             clk_i   => clk_i,
             reset_i => '0',
@@ -151,151 +152,138 @@ begin
             value_o => open,
             cycle_o => clk_slow_20ms
         );
----------------------------------------------------------------
--- Vérification fermeture porte
----------------------------------------------------------------
-    p_porte : process(switches_i(15), pause)
+
+-------------------------------------------------------------------
+-- Process principal :
+-------------------------------------------------------------------
+    p_fonctionnement_micro_ondes : process(clk_i)
     begin
-        if switches_i(15) = '0' then
-            porte_fermee <= '0';
-            pause <= '1';
-        else
-            porte_fermee <= '1';
-        end if;
-    end process p_porte;
----------------------------------------------------------------
--- Bouton de démarrage et pause
----------------------------------------------------------------
-    p_start_stop : process(btn_center_i, fonctionnement)
-    begin
-        if btn_center_i = '1' then
-            if fonctionnement = '1' then
-                fonctionnement <= '0';
+        if rising_edge(clk_i) then
+
+            ----------------------------------------------------------------
+            --        GESTION PORTE FERMÉE / PAUSE (switches_i(15))       --
+            ----------------------------------------------------------------
+            if switches_i(15) = '0' then
+                porte_fermee <= '0';
                 pause <= '1';
             else
-                fonctionnement <= '1';
-                pause <= '0';
+                porte_fermee <= '1';
             end if;
-        end if;
-    end process p_start_stop;
----------------------------------------------------------------
--- Autorisation fonctionnement
----------------------------------------------------------------
-    p_autorisation : process(fonctionnement, pause, porte_fermee)
-    begin
-        if fonctionnement = '1' and pause = '0' and porte_fermee = '1' then
-            debut <= '1';
-        else
-            debut <= '0';
-        end if;
-    end process p_autorisation;
----------------------------------------------------------------
--- Configuration du chronomètre
----------------------------------------------------------------
-    p_config_chrono : process(btn_left_i, btn_right_i, secondes)
-    begin
-        if btn_left_i = '1' and secondes > 29 then
-            secondes <= secondes - 30;
-            secondes_decalees <= secondes + 1;
-        elsif btn_right_i = '1' and secondes < 5970 then
-            secondes <= secondes + 30;
-            secondes_decalees <= secondes + 1;
-        end if;
-    end process p_config_chrono;
----------------------------------------------------------------
--- Configuration du fonctionnement du micro-ondes
----------------------------------------------------------------
-    p_fonctionnement_micro_ondes : process(clk_i, debut, clk_slow_1s)
-    begin
-        if rising_edge(clk_i) then
-            if debut = '1' then
-                if clk_slow_1s = '1' then
-                    if secondes > 0 and secondes_decalees > 1 then
-                        magnetron <= '1';
-                        secondes <= secondes - 1;
-                        secondes_decalees <= secondes_decalees - 1;
-                    elsif secondes_decalees = 1 and secondes = 0 then 
-                        magnetron <= '0';
-                        secondes <= secondes;
-                    end if;
+
+            ----------------------------------------------------------------
+            --              BOUTON START/STOP (btn_center_i)              --
+            ----------------------------------------------------------------
+            if btn_center_i = '1' then
+                if fonctionnement = '1' then
+                    -- Charlie, on pause tout ça
+                    fonctionnement <= '0';
+                    pause <= '1';
+                else
+                    -- Charlie, on remet tout en route
+                    fonctionnement <= '1';
+                    pause <= '0';
                 end if;
             end if;
-        end if;
-    end process p_fonctionnement_micro_ondes;
----------------------------------------------------------------
--- Configuration du buzzer
----------------------------------------------------------------
-    p_buzzer : process(clk_i, clk_slow_1s, secondes, secondes_decalees)
-    begin
-        if rising_edge(clk_i) then
-            if clk_slow_1s = '1' and secondes_decalees = 1 and secondes = 0 then
-                buzzer_actif <= '1';
-                compteur_buzzer <= 0;
-            elsif buzzer_actif = '1' then
+
+            ----------------------------------------------------------------
+            --       CONFIGURATION CHRONO (btn_left_i, btn_right_i)       --
+            ----------------------------------------------------------------
+            if btn_left_i = '1' then
+                if secondes > 29 then
+                    secondes <= secondes - 30; -- minimum atteignable de 0s
+                    secondes_decalees <= secondes + 1;
+                end if;
+            elsif btn_right_i = '1' then
+                if secondes < 5970 then
+                    secondes <= secondes + 30; -- maximum atteignable de 99m59s
+                    secondes_decalees <= secondes + 1;
+                end if;
+            end if;
+
+            ----------------------------------------------------------------
+            --                FONCTIONNEMENT MICRO-ONDES                  --
+            ----------------------------------------------------------------
+            if fonctionnement = '1' and pause = '0' and porte_fermee = '1' then
+                if clk_slow_1s = '1' then
+                    if secondes > 0 then
+                        magnetron <= '1';
+                        secondes  <= secondes - 1;
+                        secondes_decalees <= secondes_decalees - 1;
+                    else
+                        magnetron <= '0';
+                    end if;
+                end if;
+            else
+                magnetron <= '0';
+            end if;
+
+            ----------------------------------------------------------------
+            --                           BUZZER                           --
+            ----------------------------------------------------------------
+            if buzzer_actif = '1' then
                 if clk_slow_1s = '1' and compteur_buzzer < 3 then
                     compteur_buzzer <= compteur_buzzer + 1;
                 elsif compteur_buzzer = 3 then
-                    buzzer_actif <= '0';
+                    buzzer_actif    <= '0';
+                end if;
+            else
+                if clk_slow_1s = '1' and secondes_decalees = 1 and secondes = 0 then
+                    buzzer_actif    <= '1';
+                    compteur_buzzer <= 0;
                 end if;
             end if;
-            secondes_decalees <= secondes;
-        end if;
-    end process p_buzzer;
----------------------------------------------------------------
--- Configuration du Convertisseur
----------------------------------------------------------------
-    p_convertisseur : process (secondes)
-    begin
-        if secondes /= 0 then
-            minute <= (secondes * 34 / 64);
-            seconde <= secondes - (minute * 60);
-            dizaine_minute <= minute * 204 / 2048;
-            unite_minute <= minute - (dizaine_minute * 10);
+
+            ----------------------------------------------------------------
+            --      CONVERSION SECONDES -> 4 QUARTETS -> AFFICHEUR        --
+            ----------------------------------------------------------------
+            minute          <= secondes * 34 / 64;  -- division entière
+            seconde         <= secondes - (minute * 60);
+
+            dizaine_minute  <= minute * 204 / 2048;
+            unite_minute    <= minute - (dizaine_minute * 10);
+
             dizaine_seconde <= seconde * 204 / 2048;
-            unite_seconde <= seconde - (dizaine_seconde * 10);
+            unite_seconde   <= seconde - (dizaine_seconde * 10);
+
         end if;
-    end process p_convertisseur;
----------------------------------------------------------------
--- MUX pour l'affichage
----------------------------------------------------------------
+    end process p_fonctionnement_micro_ondes;
+
+    -------------------------------------------------------------------
+    --                       AFFECTATION LEDs                        --
+    -------------------------------------------------------------------
+    leds_o <= (others => magnetron OR buzzer_actif);
+
+    -------------------------------------------------------------------
+    --                      MUX POUR AFFICHEUR                       --
+    -------------------------------------------------------------------
     p_affichage : process(clk_slow_20ms)
     begin
-        case afficheur_selection is
-            when "00" => 
-                valeur_afficheur <= dizaine_minute;
-                disp_select_n_o <= "1110";
-            when "01" => 
-                valeur_afficheur <= unite_minute;
-                disp_select_n_o <= "1101";
-            when "10" => 
-                valeur_afficheur <= dizaine_seconde;
-                disp_select_n_o <= "1011";
-            when "11" => 
-                valeur_afficheur <= unite_seconde;
-                disp_select_n_o <= "0111";
-            when others =>
-                valeur_afficheur <= 0;
-                disp_select_n_o <= "1111";
-        end case;
+        if rising_edge(clk_slow_20ms) then
+            valeur_et_afficheur_selection <= std_logic_vector(unsigned(valeur_et_afficheur_selection) + 1 );
+        end if;
     end process p_affichage;
----------------------------------------------------------------
--- Décodage des segments pour l'affichage
----------------------------------------------------------------
+
+    -- Sélection de l'afficheur à piloter :
+    with valeur_et_afficheur_selection select
+        disp_select_n_o <= "1110"           when "00",
+                           "1101"           when "01",
+                           "1011"           when "10",
+                           "0111"           when "11";
+
+    -- Sélection de la valeur à afficher :
+    with valeur_et_afficheur_selection select
+        valeur_afficheur <= unite_seconde   when "00",
+                            dizaine_seconde when "01",
+                            unite_minute    when "10",
+                            dizaine_minute  when "11";
+
+    -------------------------------------------------------------------
+    -- Implémentation de l'afficheur 7 segments :
+    -------------------------------------------------------------------
     decoder_inst : entity work.SegmentDecoder(TruthTable)
         port map(
-            digit_i => valeur_afficheur,
+            digit_i    => valeur_afficheur,
             segments_o => disp_segments_n_o
         );
----------------------------------------------------------------
--- Sélection cyclique des afficheurs
----------------------------------------------------------------
-    p_selection_afficheur : process(clk_slow_20ms)
-    begin
-        if rising_edge(clk_slow_20ms) then
-            afficheur_selection <= std_logic_vector( unsigned(afficheur_selection) + 1 );
-        end if;
-    end process p_selection_afficheur;
-
-leds_o <= (others => magnetron OR buzzer_actif);
 
 end Structural;
